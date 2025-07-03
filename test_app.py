74668bbc4fb5d77ee394ca9f716607ffdd6445b6
shashi04 <shashivardhan04@gmail.com>
2025-07-03 20:28:35 +0530

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -3,7 +3,7 @@ from app import hello
 
 class TestApp(unittest.TestCase):
     def test_hello(self):
-        self.assertEqual(hello(), "Hello, Jenkins!")
+        self.assertEqual(hello(), "Welcome, Jenkins!")
 
 if __name__ == "__main__":
     unittest.main()
