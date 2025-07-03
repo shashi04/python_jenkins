b1b76636eb175f42bee9d38484f397e594c53965
shashi04 <shashivardhan04@gmail.com>
2025-07-03 19:08:48 +0530

    Create test_app.py

---
+++ test_app.py
@@ -0,0 +1,9 @@
+import unittest
+from app import hello
+
+class TestApp(unittest.TestCase):
+     def test_hello(self):
+         self.assertEqual(hello(), "Hello, Jenkins!")
+
+if __name__ == "__main__":
+    unittest.main()
